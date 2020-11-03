399360eadad72a17550c90198a9a9f880f9ef80c
Jason Nowkaoski <jaymanx15@gmail.com>
2020-11-03 16:36:18 -0500

    Updates

--- poeapi.go
+++ poeapi.go
@@ -1,8 +1,80 @@
 package poeapi
 
+//Socket for POE items
+type Socket struct {
+	Group   int    `json:"group"`
+	Attr    string `json:"attr"`
+	SColour string `json:"sColour"`
+}
+
+//Category of poe items
+type Category struct {
+	Accessories []string `json:"accessories"`
+	Armour      []string `json:"armour"`
+	Jewels      []string `json:"jewels"`
+	Weapons     []string `json:"weapons"`
+}
+
+type Value struct {
+	Val []string
+}
+
+// Property values supporting POE Items
+type Property struct {
+	Name        string      `json:"name"`
+	Values      interface{} `json:"values"`
+	DisplayMode int         `json:"displayMode"`
+	Type        int         `json:"type"`
+	Progress    float32     `json:"progress"`
+}
+
 //Item has all the poe fields
 type Item struct {
-	AbyssJewel bool `json:"abyssJewel"`
+	AbyssJewel            bool       `json:"abyssJewel"`
+	AdditionalProperties  []Property `json:"additionalProperties"`
+	ArtFileName           string     `json:"artFilename"`
+	Category              []Category `json:"category"`
+	Corrupted             bool       `json:"corrupted"`
+	CosmeticMods          []string   `json:"cosmeticMods"`
+	CraftedMods           []string   `json:craftedMods"`
+	DescrText             string     `json:"descrText"`
+	Duplicated            bool       `json:"duplicated"`
+	Elder                 bool       `json:"elder"`
+	EnchantMods           []string   `json:"enchantMods"`
+	ExplicitMods          []string   `json:"explicitMods"`
+	FlavourText           []string   `json:"flavourText"`
+	FrameType             int        `json:"frameType"`
+	Height                int        `json:"h"`
+	Icon                  string     `json:"icon"`
+	ID                    string     `json:"id"`
+	Identified            bool       `json:"identified"`
+	Ilvl                  int        `json:"ilvl"`
+	ImplicitMods          []string   `json:"implicitMods"`
+	InventoryID           string     `json:"inventoryId"`
+	IsRelic               bool       `json:"isRelic"`
+	League                string     `json:"league"`
+	LockedToCharacter     bool       `json:"lockedToCharacter"`
+	MaxStackSize          int        `json:"maxStackSize"`
+	Name                  string     `json:"name"`
+	NextLevelRequirements []Property `json:"nextLevelRequirements"`
+	Note                  string     `json:"note"`
+	Properties            []Property `json:"properties"`
+	ProphecyDiffText      string     `json:"prophecyDiffText"`
+	ProphecyText          string     `json:"prophecyText"`
+	Requirements          []Property `json:"requirements"`
+	SecDescrText          string     `json:"secDescrText"`
+	Shaper                bool       `json:"shaper"`
+	SocketedItems         []Property `json:"socketedItems"`
+	Sockets               []Socket   `json:"sockets"`
+	StackSize             int        `json:"stackSize"`
+	Support               bool       `json:"support"`
+	TalismanTier          int        `json:"talismanTier"`
+	TypeLine              string     `json:"typeLine"`
+	UtilityMods           []string   `json:"utilityMods"`
+	Verified              bool       `json:"verified"`
+	Width                 int        `json:"w"`
+	X                     int        `json:"x"`
+	Y                     int        `json:"y"`
 }
 
 //Stash contains all the gear in poe
